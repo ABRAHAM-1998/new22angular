63c206833ebb2a5855fe9335cb448025189dd99f
ABRAHAM-1998 <abrahamjolly3377@gmail.com>
2020-08-18 15:33:10 +0530

    first ddd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
 
-const routes: Routes = [ {path:'', component:LoginComponent, pathMatch:'full'}];
+const routes: Routes = [ {path:'login', component:LoginComponent, pathMatch:'full'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
