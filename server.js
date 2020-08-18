bf0931074e2711c60e7712ec8caff38126144f3f
ABRAHAM-1998 <abrahamjolly3377@gmail.com>
2020-08-18 15:08:05 +0530

    first ddd

---
+++ server.js
@@ -0,0 +1,10 @@
+const express  =  require('express');
+const path = require('path');
+const app = express();
+
+
+app.use(express.static(__dirname + '/dist/twentytwo22'));
+app.get('/*',function(req,res){
+    res.sendFile(path.join(__dirname + '/dist/twentytwo22/index.html'))
+});
+app.listen(process.env.PORT || 8080)
